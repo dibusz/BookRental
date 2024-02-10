127dfd5692a42ff7f0d6339abb74e70f91231341
dibidibdib <dibusz.marton@gmail.com>
2024-02-10 17:37:36 +0100

    implementing basic functionality, like add, delete, update. Implementing auto save.

---
+++ BookRentalClient/BookEditorWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookRentalClient
+{
+    /// <summary>
+    /// Interaction logic for BookEditorWindow.xaml
+    /// </summary>
+    public partial class BookEditorWindow : Window
+    {
+        public BookEditorWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Book? Book { get; set; }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Book = new Book();
+            Book.Title = TB_Title.Text;
+            Book.Author = TB_Author.Text;
+            Book.Type = TB_Type.Text;
+            Book.Release = DP_Release.SelectedDate;
+            DialogResult = true;
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(Book != null)
+            {
+                TB_Title.Text = Book.Title;
+                TB_Author.Text = Book.Author;
+                TB_Type.Text = Book.Type;
+                DP_Release.SelectedDate = Book.Release;
+            }
+        }
+    }
+}
--- BookRentalClient/MainWindow.xaml.cs
+++ BookRentalClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,65 @@ namespace BookRentalClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private const string BookDBPath = "books.csv";
+
+        private void NewBook_Click(object sender, RoutedEventArgs e)
+        {
+            BookEditorWindow window = new BookEditorWindow();
+            
+            if (window.ShowDialog() == true) 
+            {
+                LB_Books.Items.Add(window.Book);
+                Save();
+            }
+        }
+
+        private void DeleteBook_click(object sender, RoutedEventArgs e)
+        {
+            LB_Books.Items.Remove(LB_Books.SelectedItem);
+            Save();
+        }
+
+        private void EditBook_Click(object sender, RoutedEventArgs e)
+        {
+            BookEditorWindow window = new BookEditorWindow();
+            window.Book = (Book)LB_Books.SelectedItem;
+            if (window.ShowDialog() == true)
+            {
+                int index = LB_Books.SelectedIndex;
+                LB_Books.Items.RemoveAt(index);
+                LB_Books.Items.Insert(index, window.Book);
+                Save();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(BookDBPath))
+            {
+                string[] lines = File.ReadAllLines(BookDBPath);
+                foreach (string line in lines)
+                {
+                    Book book = Book.FromCSV(line);
+                    LB_Books.Items.Add(book);
+                }
+            } else
+            {
+                File.Create(BookDBPath);
+            }
+            
+        }
+
+        private void Save()
+        {
+            List<string> op = new List<string>();
+            foreach (Book item in LB_Books.Items)
+            {
+                op.Add(item.ToCSV());
+            }
+            File.WriteAllLines(BookDBPath, op);
         }
     }
 }
